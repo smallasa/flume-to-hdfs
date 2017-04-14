f6acad21a361c6e2761d447b843e32091d367b6b
liu_p <liu_p@jointwisdom.cn>
2017-04-14 16:54:27 +0800

    update

--- flume.to.hdfs/bin/start-flume.sh
+++
@@ -1,1 +0,0 @@
-nohup flume-ng agent -n consumer -f conf/consumer.properties -Dflume.monitoring.type=ganglia -Dflume.monitoring.hosts=nn2.zdp.ol:8649 &
--- flume.to.hdfs/conf/flume.sh
+++
@@ -1,1 +0,0 @@
-bin/flume-ng agent -n ssp_consumer -f conf/test.flume.2.hdfs.properties --conf conf -Dcom.sun.management.jmxremote.port=33141 &
